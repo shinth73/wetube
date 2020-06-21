8b731c1c200c090c82280eacf993432bb3b05588
shinth73 <60809725+shinth73@users.noreply.github.com>
2020-06-21 13:47:05 +0900

    20200621

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 <!-- @format -->
 
 #WeTube
+
+Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,7 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
+// userRouter.get(routes.users, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
